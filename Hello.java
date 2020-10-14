6ff72c73237c3b30bcdc719c1244b593a2d0b547
atsushi-kitazawa <atsushi.kitazawa.1987@gmail.com>
2020-10-15 01:02:44 +0900

    Add priod

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
   public static void main(String[] args) {
     String name = "hoge";
-    System.out.println("Hello " + name);
+    System.out.println("Hello " + name + ".");
   }
 }
